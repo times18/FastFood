481fba34e9fb19987ae407a64ac7f402a2747aff
times18 <timjames.lopez@gmail.com>
2023-11-17 21:44:21 +0800

    Initial Commit

---
+++ FastFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastFood
+//
+//  Created by Macbook Pro on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
