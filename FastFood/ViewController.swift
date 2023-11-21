f74cbae47a262ea7c9b9bec7ab9d06775ecfe9be
aptaclino1 <aptaclino.8@gmail.com>
2023-11-21 22:11:22 +0800

    add label

--- FastFood/ViewController.swift
+++ FastFood/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        testLabel.text = "Hello world!"
     }
 
 
